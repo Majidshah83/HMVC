d42d7e4c78a99f4c8af26150746953617eee947e
Majidshah83 <shahmajid508@gmail.com>
2022-05-31 18:54:15 +0500

    create role  give permission and also give permission to login user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,12 +6,57 @@ use App\Models\User;
 use Spatie\Permission\Models\Role;
 use DB;
 use Hash;
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Arr;
 class UserController extends Controller
 {
    public function create()
    {
-       $result=User::all();
-       dd($result);
+   // create role
+     $role = Role::create(['name' => 'admin']);
+     $role = Role::create(['name' => 'editor']);
+     $role = Role::create(['name' => 'Writer']);
+     $role = Role::create(['name' => 'Reader']);
+      return $role;
+
+   // give permission
+
+   $role = Role::findByName('Admin');
+   $role->givePermissionTo('Edit articles');
+   dd($role);
+   $role = Role::findByName('Editor');
+   $role->givePermissionTo('Edit articles');
+   dd($role);
+$role = Role::findByName('Writer');
+   $role->givePermissionTo('Write articles');
+   dd($role);
+$role = Role::findByName('Reader');
+   $role->givePermissionTo('Reader  articles');
+   dd($role);
+$role = Role::findByName('Admin');
+   $role->givePermissionTo('Write articles');
+   dd($role);
+$role = Role::findByName('Admin');
+   $role->givePermissionTo('Reader  articles');
+   dd($role);
+
+// Admin model
+ $restult =auth()->user()->givePermissionTo('Write articles');
+ $restult =auth()->user()->givePermissionTo('Edit articles');
+ $restult =auth()->user()->givePermissionTo('Reader  articles');
+dd($restult);
+
+// Writer model
+$restult =auth()->user()->givePermissionTo('Write articles');
+dd($restult);
+
+// Editor model
+$restult =auth()->user()->givePermissionTo('Edit articles');
+ dd($restult);
+
+// Reader model
+$restult =auth()->user()->givePermissionTo('Reader  articles');
+ dd($restult);
    }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::
+Route::get('create',[UserController::class, 'create'])->name('create');
